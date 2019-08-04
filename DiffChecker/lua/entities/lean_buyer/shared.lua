4ebeed8458907321c2c148ac7f5fcd5f43730d76
MorningCoffeeZombie <40343762+MorningCoffeeZombie@users.noreply.github.com>
2019-08-03 21:23:52 -0500

    Checking diff

--- DiffChecker/lua/entities/lean_buyer/shared.lua
+++
@@ -1,5 +0,0 @@
-ENT.Type = "ai"
-ENT.Base = "base_ai"
-ENT.PrintName = "Lean Buyer"
-ENT.Category = "Lean-Production"
-ENT.Spawnable = true
--- DiffChecker/lua/lean_production/cl_leanmsgs.lua
+++
@@ -1,5 +0,0 @@
-if !CLIENT then return end
-net.Receive("lean_msg", function()
-	local msg = net.ReadString()
-	chat.AddText(lean_updated.prefixcol.val,lean_updated.prefix.val .. " ",Color(255,255,255),msg)
-end)
