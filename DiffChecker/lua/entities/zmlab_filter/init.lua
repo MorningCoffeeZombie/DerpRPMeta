16163dd7126d341d009a6db0638566ccd6379454
MorningCoffeeZombie <40343762+MorningCoffeeZombie@users.noreply.github.com>
2019-08-26 20:26:46 -0500

    Adding OUR CONFIGS

---
+++ DiffChecker/lua/entities/zmlab_filter/init.lua
@@ -0,0 +1,51 @@
+AddCSLuaFile("cl_init.lua")
+AddCSLuaFile("shared.lua")
+include("shared.lua")
+
+function ENT:SpawnFunction(ply, tr)
+	if (not tr.Hit) then return end
+	local SpawnPos = tr.HitPos + tr.HitNormal * 15
+	local ent = ents.Create(self.ClassName)
+	local angle = ply:GetAimVector():Angle()
+	angle = Angle(0, angle.yaw, 0)
+	angle:RotateAroundAxis(angle:Up(), 90)
+	ent:SetAngles(angle)
+	ent:SetPos(SpawnPos)
+	ent:Spawn()
+	ent:Activate()
+	zmlab.f.SetOwner(ent, ply)
+
+	return ent
+end
+
+function ENT:Initialize()
+	self:SetModel("models/zerochain/zmlab/zmlab_filter.mdl")
+	self:PhysicsInit(SOLID_VPHYSICS)
+	self:SetSolid(SOLID_VPHYSICS)
+	self:SetMoveType(MOVETYPE_VPHYSICS)
+	self:SetUseType(SIMPLE_USE)
+	self:SetCollisionGroup(COLLISION_GROUP_WEAPON)
+	self:UseClientSideAnimation()
+	local phys = self:GetPhysicsObject()
+
+	if (phys:IsValid()) then
+		phys:Wake()
+		phys:EnableMotion(true)
+	end
+
+	zmlab.f.Filter_Initialize(self)
+end
+
+function ENT:AcceptInput(input, activator, caller, data)
+	if string.lower(input) == "use" and IsValid(activator) and activator:IsPlayer() and activator:Alive() then
+		zmlab.f.Filter_USE(self, activator)
+	end
+end
+
+function ENT:OnTakeDamage(dmg)
+	zmlab.f.Entity_OnTakeDamage(self, dmg,"Explosion")
+end
+
+function ENT:OnRemove()
+	zmlab.f.Filter_OnRemove(self)
+end
