d4c0735367482566c4a846c9e599c39ad40af3da
MorningCoffeeZombie <40343762+MorningCoffeeZombie@users.noreply.github.com>
2019-09-11 00:02:13 -0500

    A CONFESSION!

---
+++ DerpsHW/homework.ps1
@@ -0,0 +1,16 @@
+Write-Host "Switching to AddonsCore"
+cd C:\Users\William\Desktop\AddonsCore
+git checkout development
+git pull
+Write-Host "Switching to AddonsEconomy"
+cd C:\Users\William\Desktop\AddonsEconomy
+git checkout development
+git pull
+Write-Host "Switching to AddonsRP"
+cd C:\Users\William\Desktop\AddonsRP
+git checkout development
+git pull
+Write-Host "Switching to AddonsSweps"
+cd C:\Users\William\Desktop\AddonsSweps
+git checkout development
+git pull
