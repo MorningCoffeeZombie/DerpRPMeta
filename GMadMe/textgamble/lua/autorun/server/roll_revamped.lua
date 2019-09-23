eac93258e6f7153463dba1751261d4d241ee9916
MorningCoffeeZombie <ryan.jenson@wi.rr.com>
2019-09-23 14:59:54 -0500

    Minor fixex

--- GMadMe/textgamble/lua/autorun/server/roll_revamped.lua
+++ GMadMe/textgamble/lua/autorun/server/roll_revamped.lua
@@ -10,13 +10,13 @@ hook.Add( "PlayerSay", "PlayerRoll" , function( ply, text, public )	-- This migh
 	end
 end  )
 
-hook.Add( "PlayerSay", "PlayerRollJacks21" , function( ply, text, public )
+hook.Add( "PlayerSay", "PlayerRollJack21" , function( ply, text, public )
 	text = string.lower( text )
 	if (text == "/cards") or (text == "!cards") or (text == "/cards") or (text == "!cards") then 
-		blackjacksrandom = math.random( 1, 10 )
+		blackjackrandom = math.random( 1, 10 )
 			RunConsoleCommand("say", "you win")
-			--game.ConsoleCommand( "say \"happyshopping\"\n" )	-- this actually prints to chat without error and written by 'console'
-		return "[Cards:] " .. playername .. " drew " .. blackjacksrandom
+			--game.ConsoleCommand( "say "\"happyshopping\"\n" )	-- this actually prints to chat without error and written by 'console'
+		return "[Cards:] " .. playername .. " drew " .. blackjackrandom
 	end
 end  )
 
@@ -37,23 +37,26 @@ end  )
 --[[ ALTERNATE TEST CODE:
 
 
-hook.Add( "PlayerSay", "TextGamble" , function( Player, text, public )
+hook.Add( "PlayerSay", "TextGamble" , function( ply, text, public )
 	text = string.lower( text )
-	local playername = ply:GetName()
+	local gamble_playername = ply:GetName()
+
 	if (text == "/roll") or (text == "!roll") then 
-		return "[Roll:] " .. playername .. " rolled " .. math.random( 1, 100 ) 
-			
-	elseif (text == "/cards") or (text == "!cards") or (text == "/cards") or (text == "!cards") then 
-		local blackjacksrandom = math.random( 1, 10 )
-RunConsoleCommand("say", "testing")
-		return "[Cards:] " .. playername .. " drew " .. blackjacksrandom
-			
-	elseif (text == "/coin") or (text == "!coin") then 
+		return "[Roll:] " .. gamble_playername .. " rolled " .. math.random( 1, 100 ) 
+	end
+
+	if (text == "/cards") or (text == "!cards") or (text == "/cards") or (text == "!cards") then 
+		local blackjackrandom = math.random( 1, 10 )
+		RunConsoleCommand("say", "testing")
+		return "[Cards:] " .. gamble_playername .. " drew " .. blackjackrandom
+	end
+
+	if (text == "/coin") or (text == "!coin") then 
 		local coinresult = math.random( 0, 1 ) 
 		if (coinresult == 0) then 
-			return "[Coin:] " .. playername .. " flipped HEADS"
+			return "[Coin:] " .. gamble_playername .. " flipped HEADS"
 		else
-			return "[Coin:] " .. playername .. " flipped TAILS"
+			return "[Coin:] " .. gamble_playername .. " flipped TAILS"
 		end
 	end
 end  )
@@ -64,3 +67,81 @@ end  )
 
 
 --]]
+
+
+
+
+arr_listofcards = { "Ace of Hearts",
+					"King of Hearts",
+					"Queen of Hearts",
+					"Jack of Hearts",
+					"Ten of Hearts",
+					"Nine of Hearts",
+					"Eight of Hearts",
+					"Seven of Hearts",
+					"Six of Hearts",
+					"Five of Hearts",
+					"Four of Hearts",
+					"Three of Hearts",
+					"Two of Hearts",
+					"Ace of Clubs",
+					"King of Clubs",
+					"Queen of Clubs",
+					"Jack of Clubs",
+					"Ten of Clubs",
+					"Nine of Clubs",
+					"Eight of Clubs",
+					"Seven of Clubs",
+					"Six of Clubs",
+					"Five of Clubs",
+					"Four of Clubs",
+					"Three of Clubs",
+					"Two of Clubs",
+					"Ace of Spades",
+					"King of Spades",
+					"Queen of Spades",
+					"Jack of Spades",
+					"Ten of Spades",
+					"Nine of Spades",
+					"Eight of Spades",
+					"Seven of Spades",
+					"Six of Spades",
+					"Five of Spades",
+					"Four of Spades",
+					"Three of Spades",
+					"Two of Spades",
+					"Ace of Diamonds",
+					"King of Diamonds",
+					"Queen of Diamonds",
+					"Jack of Diamonds",
+					"Ten of Diamonds",
+					"Nine of Diamonds",
+					"Eight of Diamonds",
+					"Seven of Diamonds",
+					"Six of Diamonds",
+					"Five of Diamonds",
+					"Four of Diamonds",
+					"Three of Diamonds",
+					"Two of Diamonds"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
