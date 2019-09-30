2bee364c8754834099ade0d6d8c6bd2ca2493967
MorningCoffeeZombie <40343762+MorningCoffeeZombie@users.noreply.github.com>
2019-09-30 00:45:28 -0500

    diff checking

--- DiffChecker/lua/autorun/server/fadingdoortoolvehicle.lua
+++
@@ -1,5 +0,0 @@
-hook.Add("CanTool", "Fading_Door_Deny", function(ply, tr, tool)
-    if tool == "Your tool name" and tr.Entity:IsVehicle() and !(ply:IsAdmin() or ply:IsSuperAdmin()) then
-        return false
-    end
-end)
